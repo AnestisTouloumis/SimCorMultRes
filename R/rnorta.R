ece17a0fa16479b0cb49d4d4fe4256b5774ffbd8
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2017-06-13 17:17:15 +0100

    Installing SimCorMultRes to GitHub

---
+++ R/rnorta.R
@@ -0,0 +1,23 @@
+rnorta <- function(R = R, cor.matrix = cor.matrix, distr = distr, qparameters = NULL) {
+    if (all.equal(R, as.integer(R)) != TRUE | R < 1) 
+        stop("'R' must be a positive integer")
+    QuantileFunctions <- as.character(distr)
+    ans <- rsmvnorm(R = R, cor.matrix = cor.matrix)
+    if (length(QuantileFunctions) != ncol(cor.matrix)) 
+        stop("'distr' must be a ", ncol(cor.matrix), "-variate vector of strings naming a valid quantile function")
+    if (!is.null(qparameters)) {
+        qparameters <- as.list(qparameters)
+        if (length(qparameters) != ncol(cor.matrix)) 
+            stop("'qparameters' must be provided as a list of length ", ncol(cor.matrix))
+    }
+    ans <- pnorm(ans)
+    for (i in 1:ncol(cor.matrix)) {
+        QuantileFunction <- get(QuantileFunctions[i], mode = "function")
+        if (!is.function(QuantileFunction)) 
+            stop("Character string ", i, " in `distr' does not correspond to a valid function")
+        if (!is.null(qparameters)) 
+            formals(QuantileFunction)[pmatch(names(qparameters[[i]]), formalArgs(QuantileFunction))] <- qparameters[[i]]
+        ans[, i] <- QuantileFunction(ans[, i])
+    }
+    ans
+}
---
+++ R/rsmvnorm.R
@@ -0,0 +1,18 @@
+rsmvnorm <- function(R = R, cor.matrix = cor.matrix) {
+    if (all.equal(R, as.integer(R)) != TRUE | R < 1) 
+        stop("'R' must be a positive integer")
+    if (!is.numeric(cor.matrix)) 
+        stop("'cor.matrix' must be numeric")
+    cor.matrix <- as.matrix(cor.matrix)
+    if (!isSymmetric(cor.matrix)) 
+        stop("'cor.matrix' must be a symmetric matrix")
+    if (any(diag(cor.matrix) != 1)) 
+        stop("the diagonal elements of 'cor.matrix' must be equal to one")
+    if (any(cor.matrix > 1) | any(cor.matrix < -1)) 
+        stop("all the elements of 'cor.matrix' must be on [-1,1]")
+    if (any(eigen(cor.matrix, symmetric = TRUE, only.values = TRUE)$values <= 0)) 
+        stop("'cor.matrix' must be a positive definite matrix")
+    p <- ncol(cor.matrix)
+    ans <- matrix(rnorm(R * p), R, p) %*% chol(cor.matrix)
+    ans
+}
