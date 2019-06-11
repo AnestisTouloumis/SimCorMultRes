013f3ee38c51f9cd22a2ec168e293ecd7910db3c
AnestisTouloumis <A.Touloumis@brighton.ac.uk>
2019-06-11 13:25:41 +0100

    Adding unit testing for rmult.crm

--- tests/testthat/test_core_functions.R
+++ tests/testthat/test_core_functions.R
@@ -135,3 +135,24 @@ test_that("rmult.acl constant betas",{
   expect_equal(c(t(CorOrdRes$Ysim)), c(t(CorNomRes$Ysim)))
 })
 
+
+test_that("rmult.crm constant betas",{
+  N <- 10
+  clsize <- 4
+  intercepts <- c(-1.5, -0.5, 0.5, 1.5)
+  betas <- 1
+  x <- rnorm(N * clsize)
+  ncategories <- 5
+  cor.matrix <- diag(1, (ncategories - 1) * clsize) +
+    kronecker(toeplitz(c(0, rep(0.24, ncategories - 2))),
+              matrix(1, clsize, clsize))
+  CorOrdRes <- rmult.crm(clsize = clsize, intercepts = intercepts, betas = betas,
+                         xformula = ~x, cor.matrix = cor.matrix, link = 'probit')
+  U <- c(t(CorOrdRes$rlatent)) - rep(x, each = ncategories - 1)
+  Ysim <- matrix(as.numeric(t(U <= intercepts)), N * clsize, ncategories -
+                   1, TRUE)
+  for (i in 1:(ncategories - 1)) Ysim[, i] <- ifelse(Ysim[, i] ==
+                                                       1, i, ncategories)
+  Ysim <- apply(Ysim, 1, min)
+  expect_equal(c(t(CorOrdRes$Ysim)), Ysim)
+})
